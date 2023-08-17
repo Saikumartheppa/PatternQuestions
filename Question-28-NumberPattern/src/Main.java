5daba55e044c61d74507e1e96b6d220cfbbd67a5
SaiKumarTheppa <saikumartheppao6@gmail.com>
2023-08-17 17:25:39 +0530

    Question-28 solution is corrected

--- Question-28-NumberPattern/src/Main.java
+++ Question-28-NumberPattern/src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
 //        Given int n = 5.You have to print the output as given below.
-//                          1
-//                      2 	3	2
-//                  3   4   5	4	3
-//              4	5	6	7	6	5	4
-//          5   6   7	8	9	8	7	6	5
+//                            1
+////                      2   3   2
+////                  3   4   5   4   3
+////              4   5   6   7   6   5   4
+////          5   6   7   8   9   8   7   6   5
 
 
 //        int n = 5, num = 1;
